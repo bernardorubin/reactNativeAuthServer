775336d9f7cfdda702a6597d85f745c4cabbfd01
Bernardo Emilio Rubin de Celis Martinez <bernulander@gmail.com>
2018-07-13 01:04:37 -0500

    Compared password vs hashedPassword with bcrypt

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
 const mongoose = require('./db/mongoose');
+const bcrypt = require("bcrypt");
+
+let password = 'myPassword';
+let salt = bcrypt.genSaltSync(12);
+let hashedPassword = bcrypt.hashSync(password, salt);
+console.log(`My hashed password is equal to -> ${hashedPassword}`);
+let compare = bcrypt.compareSync(password, hashedPassword);
+console.log(`If true the password compared to hashed result are equal -> ${compare}`);
 
 app.get('/', (req, res) => {
   res.send('welcome');
