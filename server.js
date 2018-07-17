50113cb693e77376b390930e5ef776437ba3f652
Bernardo Emilio Rubin de Celis Martinez <bernulander@gmail.com>
2018-07-17 17:56:54 -0500

    Setup login and register routes

--- server.js
+++ server.js
@@ -1,18 +1,26 @@
 const express = require('express');
 const app = express();
 const mongoose = require('./db/mongoose');
-const bcrypt = require("bcrypt");
+const userRoutes = require('./routes/user-routes');
+// parses json requests and places it in a body object
+const bodyParser = require('body-parser');
 
-let password = 'myPassword';
-let salt = bcrypt.genSaltSync(12);
-let hashedPassword = bcrypt.hashSync(password, salt);
-console.log(`My hashed password is equal to -> ${hashedPassword}`);
-let compare = bcrypt.compareSync(password, hashedPassword);
-console.log(`If true the password compared to hashed result are equal -> ${compare}`);
+app.use(bodyParser.json());
+
+// const bcrypt = require("bcrypt");
+
+// let password = 'myPassword';
+// let salt = bcrypt.genSaltSync(12);
+// let hashedPassword = bcrypt.hashSync(password, salt);
+// console.log(`My hashed password is equal to -> ${hashedPassword}`);
+// let compare = bcrypt.compareSync(password, hashedPassword);
+// console.log(`If true the password compared to hashed result are equal -> ${compare}`);
 
 app.get('/', (req, res) => {
   res.send('welcome');
 })
+
+app.use('/user', userRoutes);
 
 const PORT = 3000;
 
